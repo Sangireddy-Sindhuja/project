a5ce3d59f88325ccebf453f9ed6feb877f009b85
Sangireddy-Sindhuja <sangireddysindhu6@gmail.com>
2025-06-30 15:07:12 +0530

    Add files via upload

---
+++ client/src/pages/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Programs from './pages/Programs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/programs" element={<Programs />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
